5a4636a44b26543a052fe234e6744bb7f26534b1
DCsunset <DCsunset@protonmail.com>
2023-08-22 21:33:32 -0400

    fix(overlays/pkgs): fix import

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -6,7 +6,7 @@ self: super:
 let
   isReserved = n: n == "lib" || n == "overlays" || n == "modules";
   nameValuePair = n: v: { name = n; value = v; };
-  nurAttrs = import ./default.nix { pkgs = super; };
+  nurAttrs = import ../default.nix { pkgs = super; };
 in
 {
   # add packages to a namespace
