bf4be3340c953a899d7f3b9e7851791e58769277
DCsunset <DCsunset@protonmail.com>
2023-12-26 14:39:09 -0500

    rangefs: register with mount.fuse

--- pkgs/rangefs/default.nix
+++ pkgs/rangefs/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, rustPlatform }:
+{ lib, fuse3, makeWrapper, fetchFromGitHub, rustPlatform }:
 
 rustPlatform.buildRustPackage rec {
   pname = "rangefs";
@@ -11,7 +11,17 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-xN/OFYxWVEke0YQCUoCcBAjflJwIfW9Y0XNQpnCsIl0=";
   };
 
+  nativeBuildInputs = [
+    makeWrapper
+  ];
   cargoHash = "sha256-gmSwg3ahWT0wUCIAr88O6RaHGewbxoC/JGIP2bKsBxg=";
+
+  # reference: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/filesystems/gocryptfs/default.nix
+  postInstall = ''
+    wrapProgram $out/bin/rangefs \
+      --suffix PATH : ${lib.makeBinPath [ fuse3 ]}
+    ln -s $out/bin/rangefs $out/bin/mount.fuse.rangefs
+  '';
 
   meta = with lib; {
     description = "A fuse-based filesystem to map ranges in file to individual files.";
