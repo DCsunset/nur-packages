2d3dbbf9694f2a75295ac99737510d55ec56f03c
DCsunset <DCsunset@protonmail.com>
2024-01-08 18:40:45 -0500

    rangefs: 0.4.2 -> 0.4.3
    Diff: https://github.com/DCsunset/rangefs/compare/v0.4.2...v0.4.3

--- pkgs/rangefs/default.nix
+++ pkgs/rangefs/default.nix
@@ -2,19 +2,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rangefs";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "DCsunset";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Pzj55MXlCm4kuruCh9oRyci0wEjVfGbqWJFdtvDaJ9I=";
+    hash = "sha256-BSkU103k91S2EPkbfQzB88xxWepSPBRDTKPUds0M6YM=";
   };
 
   nativeBuildInputs = [
     makeWrapper
   ];
-  cargoHash = "sha256-zTTTgbLiRyQJN2ebK9pI5JD2UcE6lixt7n6CUrdOwWI=";
+  cargoHash = "sha256-AWZcposd3n4+qGD7quEnNpQxnxW12jX2O1yCf09y2Ig=";
 
   # reference: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/filesystems/gocryptfs/default.nix
   postInstall = ''
