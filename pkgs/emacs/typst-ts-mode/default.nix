f1e9972a4d9486707629b096290be2f3814c6ea6
DCsunset <DCsunset@protonmail.com>
2025-03-31 17:52:14 +0000

    emacsPackages.typst-ts-mode: 0-unstable-2025-02-21 -> 0-unstable-2025-03-18
    Diff: https://codeberg.org/meow_king/typst-ts-mode.git/compare/34d522c0a0d8eec9a8b3a6855cf394e7d5c8fb84...e0542e3e42c55983282115a97c13c023a464ff00

--- pkgs/emacs/typst-ts-mode/default.nix
+++ pkgs/emacs/typst-ts-mode/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation {
   pname = "modaled";
-  version = "0-unstable-2025-02-21";
+  version = "0-unstable-2025-03-18";
 
   src = fetchgit {
     url = "https://codeberg.org/meow_king/typst-ts-mode.git";
-    rev = "34d522c0a0d8eec9a8b3a6855cf394e7d5c8fb84";
-    hash = "sha256-hx6soqaqyk678vn3LZgkagMwsYOZaMh9TMV3hsJWukI=";
+    rev = "e0542e3e42c55983282115a97c13c023a464ff00";
+    hash = "sha256-P/6Z4HYwN6A7bcXEiNruv2/NHaoI7DJwYXdJ2z3VEG0=";
   };
 
   buildInputs = [
